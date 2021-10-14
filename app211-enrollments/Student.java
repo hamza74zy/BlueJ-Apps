29c8c6321cd0cf140d37914c69ce0ecf62f226d2
hamza74zy <91318851+hamza74zy@users.noreply.github.com>
2021-10-14 23:29:43 +0100

    screenshot

--- app211-enrollments/Student.java
+++ app211-enrollments/Student.java
@@ -1,4 +1,4 @@
-/**
+ /**
  * The Student class contains essential information that identifies 
  * a student at BNU.  The class also contains information 
  * concerning the course the student is currently enrolled on
